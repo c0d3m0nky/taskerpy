19ccf1e000c27348abc3819621f7ba4936263ec2
c0d3m0nk3y <qbanbpc@gmail.com>
2022-07-06 00:21:46 -0400

    Moved tasker from unraidpy

---
+++ api.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from tasker.src.tbay.controllers import root, threads, settings
+from qbittorrent import root as qbittorrent
+
+router = APIRouter()
+
+#TBayMod
+router.include_router(threads.router)
+router.include_router(root.router)
+router.include_router(settings.router)
+
+#qBittorrent
+router.include_router(qbittorrent.router)
---
+++ tbay/db.py
@@ -0,0 +1,34 @@
+from os import environ as env
+import psycopg2
+from psycopg2 import extensions, extras
+
+psycopg2.extensions.register_adapter(dict, psycopg2.extras.Json)
+
+
+def connect():
+    return psycopg2.connect(user=env['PGUSER'],
+                            password=env['PGPWD'],
+                            host=env['PGHOST'],
+                            port=env['PGPORT'],
+                            database=env['PGDB'])
+
+
+def dispose(conn, cur):
+    if conn:
+        if cur:
+            cur.close()
+        conn.close()
+
+
+def test_db():
+    conn = None
+    cur = None
+    try:
+        conn = connect()
+        cur = conn.cursor()
+        cur.execute('select count(1) from public.threads')
+    except (Exception, psycopg2.Error) as error:
+        raise
+    finally:
+        dispose(conn, cur)
+
