b7e89a7a64dc7e6e84596aa896a2f5665a48e817
c0d3m0nk3y <qbanbpc@gmail.com>
2023-09-03 14:14:57 -0400

    notify prep

--- src/notify/model.py
+++ src/notify/model.py
@@ -1,8 +1,25 @@
 from typing import List, Union
 from dataclasses import dataclass
+from enum import Enum
+from furl import furl
 
 
 @dataclass
 class Message:
     to: Union[List[str], str]
     text: str
+
+
+class Unraid_Severity(Enum):
+    NORMAL = 'normal'
+    WARN = 'warning'
+    ERR = 'alert'
+
+
+@dataclass
+class Unraid_Notification:
+    severity: Unraid_Severity   # -i
+    event: str                  # -e
+    subject: str                # -s
+    description: str            # -d
+    link: furl                  # -l
