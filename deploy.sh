b0c49a2192f0aaaa639170510c97c295b8403975
c0d3m0nk3y <4998029+c0d3m0nky@users.noreply.github.com>
2025-08-25 01:24:44 -0400

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-docker build -t c0d3m0nk3y/taskerpy:latest ./
-docker tag c0d3m0nk3y/taskerpy localhost:5000/c0d3m0nk3y/taskerpy
-docker push localhost:5000/c0d3m0nk3y/taskerpy
+docker build -t d.espacebyte.net/c0d3m0nk3y/taskerpy:latest ./
+docker push d.espacebyte.net/c0d3m0nk3y/taskerpy
